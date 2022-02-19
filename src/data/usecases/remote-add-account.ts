b6292226c9c9d4671a5a9898a38c42be5eba8459
maaxg <maxgps5@hotmail.com>
2022-02-19 15:12:59 -0300

    feat: data layer created with remote add account class

---
+++ src/data/usecases/remote-add-account.ts
@@ -0,0 +1,30 @@
+import {EmailInUseError} from '../../domain/errors';
+import {AddAccount} from '../../domain/usecases';
+import {HttpClient, HttpStatusCode} from '../protocols/http';
+
+export class RemoteAddAccount implements AddAccount {
+  constructor(
+    private readonly url: string,
+    private readonly httpClient: HttpClient<RemoteAddAccount.Model>,
+  ) {}
+  async add(params: AddAccount.Params): Promise<AddAccount.Model> {
+    const httpResponse = await this.httpClient.request({
+      url: this.url,
+      method: 'post',
+      body: params,
+    });
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:
+        return httpResponse.body;
+      case HttpStatusCode.forbidden:
+        throw new EmailInUseError();
+      default:
+        throw new Error();
+    }
+  }
+}
+
+export namespace RemoteAddAccount {
+  export type Model = AddAccount.Model;
+}
