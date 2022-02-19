1d080f8d533342127c66e21ed10f542c2a602cff
maaxg <maxgps5@hotmail.com>
2022-02-19 15:59:35 -0300

    feat: insfrastructure layer created with axios http client

---
+++ src/Infrastructure/axios-http-client.ts
@@ -0,0 +1,18 @@
+import axios, {AxiosResponse} from 'axios';
+import {HttpClient, HttpRequest, HttpResponse} from '../data/protocols/http';
+
+export class AxiosHttpClient implements HttpClient {
+  async request(data: HttpRequest): Promise<HttpResponse> {
+    let axiosResponse: AxiosResponse;
+    try {
+      axiosResponse = await axios.request({...data});
+    } catch (error: any) {
+      axiosResponse = error.response;
+    }
+
+    return {
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data,
+    };
+  }
+}
