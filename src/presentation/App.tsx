d9f68b52e315f4efec3806896bea359d66e40ee7
maaxg <maxgps5@hotmail.com>
2022-02-19 16:15:39 -0300

    feat: presentation was created with signup page

---
+++ src/presentation/App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import SignUp from './screens/signup';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <SignUp />
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ src/presentation/screens/signup/index.tsx
@@ -0,0 +1,24 @@
+import {TextInput, View} from 'react-native';
+import React from 'react';
+// import {AddAccount} from '../../../domain/usecases';
+// import {Validation} from '../../protocols';
+/*
+type Props = {
+  validation: Validation;
+  addAccount: AddAccount;
+};
+ */
+const SignUp: React.FC = () => {
+  return (
+    <View>
+      <TextInput />
+      <TextInput />
+      <TextInput />
+      <TextInput />
+      <TextInput />
+      <TextInput />
+    </View>
+  );
+};
+
+export default SignUp;
